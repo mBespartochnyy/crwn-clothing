160cb17dc0a9f9f1fc221397380a1cec3d5a5db2
Mikhail Bespartochnyy <mbespartochnyy@gmail.com>
2022-01-09 16:42:13 -0500

    update our menu-items to navigate withRouter Higher Order Component (HOC)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+	<div>
+		<h1>
+			HATS PAGE
+		</h1>
+	</div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+		{/*
+			Switch routes to the first available route it finds when a user goes to some URL.
+			See Routing In Out Project lecture of Section 5: Master Project: Reach Router and Routing
+		 */}
+		<Switch>
+			<Route exact path='/' component={HomePage} />
+			<Route path ='/shop/hats' component={HatsPage} />
+		</Switch>
     </div>
   );
 }
