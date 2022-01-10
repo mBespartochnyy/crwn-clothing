845cff76eeeedc84c0706714a2b5afe767222cab
Mikhail Bespartochnyy <mbespartochnyy@gmail.com>
2022-01-09 22:42:40 -0500

    added content and styling to sign-in section of sign in and sign up page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.components';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -23,7 +24,8 @@ function App() {
 		<Switch>
 			<Route exact path='/' component={HomePage} />
 			{/* <Route path ='/shop/hats' component={HatsPage} /> */}
-			<Route path ='/shop' component={ShopPage} />
+			<Route path='/shop' component={ShopPage} />
+			<Route path='/signin' component={SignInAndSignUpPage} />
 		</Switch>
     </>
   );
