65aa88fac91c6eb8dd61750b1062bfca876deb56
Mikhail Bespartochnyy <mbespartochnyy@gmail.com>
2022-01-09 21:27:36 -0500

    adding header component and links to home, shop page, and contact page

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.components';
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <div>
+    <>
+		{/*
+			NOTE: Header component is strategically placed outside of routes so that it would show
+			up on every page.
+		*/}
+		 <Header />
 		{/*
 			Switch routes to the first available route it finds when a user goes to some URL.
-			See Routing In Out Project lecture of Section 5: Master Project: Reach Router and Routing
+			See Routing In Out Project lecture of Section 5: Master Project: Reach Router and
+			Routing.
 		 */}
 		<Switch>
 			<Route exact path='/' component={HomePage} />
 			{/* <Route path ='/shop/hats' component={HatsPage} /> */}
 			<Route path ='/shop' component={ShopPage} />
 		</Switch>
-    </div>
+    </>
   );
 }
 
