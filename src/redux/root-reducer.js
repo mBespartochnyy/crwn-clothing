d9851c320b42b8b4e45c935c772748bffa85c930
Mikhail Bespartochnyy <mbespartochnyy@gmail.com>
2022-01-17 22:32:48 -0500

    all the work in sections 9, 10, and 11

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,31 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
+// import local storage. This tells Redux "I want to use local storage as my default storage".
+import storage from "redux-persist/lib/storage";
+
+// this would need to be uncommented if session storage is needed.
+// import sessionStorage from "redux-persist/es/storage/session";
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+const persistConfig = {
+	key: 'root',
+	storage,
+	// whitelist is an array containing names of reducers we want to store.
+	// user is being handled by firebase authentication so there's no reason for us to persist it.
+	// this tells redux-persist the only reducer that we want to persist is the cart.
+	whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
-	cart: cartReducer
+	cart: cartReducer,
+	directory: directoryReducer,
+	shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
